c1c488499d632b02e31eef3d694d9420fa5c08bd
Sebastian Hardt <tuxburner@boggo.de>
2018-01-09 15:05:59 +0100

    #14 remove Expect header from curl sample

--- sampledata/Upload.md
+++ sampledata/Upload.md
@@ -1,1 +1,1 @@
- curl -i -X POST -H "Content-Type: multipart/form-data" -F "fileinfo=@2.mp3" http://192.168.0.116/upload
+ curl -i -X POST -H Expect: -H "Content-Type: multipart/form-data" -F "fileinfo=@2.mp3" http://192.168.0.116/upload
--- soundboard_code/soundboard_code.ino
+++ soundboard_code/soundboard_code.ino
@@ -196,20 +196,7 @@ void loop() {
 //                              INIT THE HTTP SERVER                                               *
 //**************************************************************************************************
 void initHttpServer() {
-
   httpServer.begin();
-
-  //httpServer.end();
-  //httpServer.reset();
-
-  // Handle file from FS
-  //httpServer.onNotFound(handleFS);
-  // Handle file uploads
-  //httpServer.onFileUpload(handleFileUpload);
-  // Handle startpage
-  //httpServer.on ( "/", handleCmd ) ;
-
-  ///httpServer.begin();
 }
 
 
@@ -306,27 +293,11 @@ void httpServerLoop() {
   while (client.connected()) {            // loop while the client's connected
     if (client.available()) {             // if there's bytes to read from the client,
       char c = client.read();             // read a byte, then
+
       if (c == '\n') {                    // if the byte is a newline character
 
-        // if the current line is blank, you got two newline characters in a row.
-        // that's the end of the client HTTP request, so send a response:
-        /*if (currentLine.length() == 0) {
-          // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-          // and a content-type so the client knows what's coming, then a blank line:
-          client.println(httpHeaderToSend);
-          client.println("Content-type:text/html");
-          client.println();
 
-          // the content of the HTTP response follows the header:
-          //client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
-          client.print(contentToSend);
-
-          // The HTTP response ends with another blank line:
-          client.println();
-          // break out of the while loop:
-          break;
-          } else {    // if you got a newline, then clear currentLine:*/
-        dbg.print("Http: Client send line: '%s'", currentLine.c_str());
+        dbg.print("Http: Client send line: %s", currentLine.c_str());
 
         // Check to see if the client request was "GET /H" or "GET /L":
         if (currentLine.startsWith("GET /play/") && httpClientAction == NONE) {
@@ -351,8 +322,7 @@ void httpServerLoop() {
           httpClientAction = FAILURE;
         }
 
-        currentLine = "";
-        //}
+        currentLine = ""; // empty the current line
       } else if (c != '\r') {  // if you got anything else but a carriage return character,
         currentLine += c;      // add it to the end of the currentLine
       }
